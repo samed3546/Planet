5c9ff20d2023952fdabd32e4585f9aca0e3d8daf
samed3546 <samed3546@hotmail.com>
2019-11-20 17:47:55 +0300

    Initial commit

---
+++ Assets/Kodes/CameraMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Vector3[] Points; // 20 birimlik konumlar
+    private int Index = 0; // mevcutkonum id
+
+    [SerializeField]
+    private float speed = 2f; // konumlar arası geçiş hızı
+    void Update()
+    {
+        Vector3 CurrentPos = Points[Index];
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            if (Index < Points.Length - 1)
+            {
+                Index++;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            if (Index > 0)
+            {
+                Index--;
+            }
+        }
+        transform.position = Vector3.Lerp(transform.position, CurrentPos, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Kodes/PlanetRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlanetRotate : MonoBehaviour 
+{
+    [SerializeField]
+    private float Rotspeed = 10f;
+
+    [System.Obsolete]
+    private void OnMouseDrag()
+    {
+        float Xrot = Input.GetAxis("Mouse X") * Rotspeed * Mathf.Deg2Rad;
+        float Yrot = Input.GetAxis("Mouse Y") * Rotspeed * Mathf.Deg2Rad;
+
+        transform.RotateAround(Vector3.up, -Xrot);
+        transform.RotateAround(Vector3.right, Yrot);
+    }
+
+}
